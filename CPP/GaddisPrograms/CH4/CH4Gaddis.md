40570d60dfde7550862b73ba8a0e881a757f0bf5
Drixitel <mpmunoz1993@gmail.com>
2022-10-01 14:03:40 -0700

    somethign

--- CPP/GaddisPrograms/CH4/CH4Gaddis.md
+++ CPP/GaddisPrograms/CH4/CH4Gaddis.md
@@ -1,7 +1,16 @@
 # Gaddis CH4
+
 Read only: `4.1, 4.5, 4.7, 4.9, 4.10, 4.13, 4.16`
-   - skim `4.12`
-# CH4: Making decisions 
-# Relational Operators 
-- relational operators 
-  - let you compare numeric and `char` values and determine whether one is >,<, =, != to one another. 
+
+- skim `4.12`
+
+# CH4: Making decisions
+
+# Relational Operators
+
+- relational operators
+  - let you compare numeric and `char` values and determine whether one is >,<, =, != to one another.
+
+# Menu Driven Progams
+
+- a `menu` is a set of choicespresented to the user
---
+++ CPP/GaddisPrograms/CH4/Program_4_10.cpp
@@ -0,0 +1,23 @@
+/* This menu-driven progam uses an if/else statement to carrry
+out the correct set of actions based on the user's menu choice
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Constants for membershiop rates
+    const double ADULT_RATE = 120.0;
+    const double CHILD_RATE = 60.0;
+    const double SENIOR_RATE = 100.0;
+
+    int choice;
+    int months;
+    double charges;
+
+    // Display the menu and get the user's choice
+    cout << "   Health Club Membership Menu \n\n";
+    cout <<
+}
